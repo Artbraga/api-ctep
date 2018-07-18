d1b1fec8398e6da357eaafbbf3c7efb5fd493475
Arthur Braga <ar.bastos@hotmail.com>
2018-07-18 05:33:18 -0300

    Commit inicial

---
+++ src/main/java/com/arthur/apiCTEP/ApiCtepApplication.java
@@ -0,0 +1,12 @@
+package com.arthur.apiCTEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCtepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCtepApplication.class, args);
+	}
+}
