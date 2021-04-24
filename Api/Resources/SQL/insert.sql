8648096554eac28af9a85c066defb7f005df55ce
Arthur Bastos Braga Coelho <arthur.coelho@radixeng.com.br>
2021-04-24 10:01:35 -0300

    alteração de banco para inclusão de permissões

--- Api/Resources/SQL/insert.sql
+++ Api/Resources/SQL/insert.sql
@@ -6,9 +6,9 @@ INSERT INTO tb_tpstatus_turma (id_tpstatus_turma, nome) VALUES
 INSERT INTO tb_tpstatus_aluno (id_tpstatus_aluno, nome) VALUES
     (1, 'Ativo'), (2, 'Concluído'), (3, 'Trancado'), (4, 'Abandono');
 
-INSERT INTO tb_usuario (nome, login, senha, telefone) VALUES
-  ('Administrador', 'admin', MD5('admin'), 00000000),
-  ('User', 'user', MD5('1234'), 11111111);
+INSERT INTO tb_usuario (nome, login, senha, telefone, id_perfil) VALUES
+  ('Administrador', 'admin', MD5('admin'), 00000000, 1),
+  ('User', 'user', MD5('1234'), 11111111, 1);
 
 INSERT INTO tb_curso (nome, sigla, sigla_turma, flg_especializacao, id_curso_vinculado) VALUES 
 	('Técnico de Enfermagem', 'ENF', 'TENF', FALSE, NULL), 
@@ -35,3 +35,28 @@ INSERT INTO tb_disciplina (nome, id_curso) VALUES
   ('Prova Módulo 2', 2),
   ('Escovação e Paramentação', 2),
   ('Mesa de Mayo', 2);
+
+INSERT INTO tb_permissao (id_permissao, nome) VALUES
+  (1, 'Consultar Alunos'),
+  (2, 'Manter Alunos'),
+  (3, 'Manter Notas'),
+  (4, 'Consultar Turmas'),
+  (5, 'Manter Turmas'),
+  (6, 'Acesso Desktop'),
+  (7, 'Acesso Portal'),
+  (8, 'Consultar Usuários'),
+  (9, 'Manter Usuários'),
+  (10, 'Consultar Professores'),
+  (11, 'Manter Professores');
+
+INSERT INTO tb_perfil (id_perfil, nome) VALUES
+  (1, 'Direção'),
+  (2, 'Secretaria'),
+  (3, 'Professor'),
+  (4, 'Aluno');
+
+INSERT INTO tb_perfil_permissao (id_perfil, id_permissao) VALUES
+  (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10), (1, 11),
+  (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 10),
+  (3, 1), (3, 3), (3, 4), (3, 6), (3, 7), 
+  (4, 7);
