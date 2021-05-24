4d15f660b4b59cffcf807fd9b58cd1ae2eda169f
Arthur Bastos Braga Coelho <arthur.coelho@radixeng.com.br>
2021-05-24 09:16:07 -0300

    busca de notas por turma

---
+++ Entities/DTOs/AlunoNotasDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class AlunoNotasDTO
+    {
+        public int IdAluno { get; set; }
+        public string NomeAluno { get; set; }
+        public string Matricula { get; set; }
+        public IEnumerable<NotaAlunoDTO> Notas { get; set; }
+    }
+}
