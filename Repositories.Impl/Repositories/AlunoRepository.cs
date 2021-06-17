e62385072d3093702a0aad6b211726a55a34a51d
Arthur Bastos Braga Coelho <arthur.coelho@radixeng.com.br>
2021-06-17 19:20:11 -0300

    Correção do filtro de aluno não vinculado

--- Repositories.Impl/Repositories/AlunoRepository.cs
+++ Repositories.Impl/Repositories/AlunoRepository.cs
@@ -43,12 +43,15 @@ namespace Repositories.Impl.Repositories
             {
                 query = query.Where(x => x.CPF.Equals(filter.CPF));
             }
-            query = query.Where(x => x.TurmasAluno.Any(y =>
-                (string.IsNullOrEmpty(filter.CodigoTurma) || y.Turma.Codigo.Contains(filter.CodigoTurma)) &&
-                (!filter.CursoId.HasValue || y.Turma.CursoId == filter.CursoId) &&
-                (string.IsNullOrEmpty(filter.Matricula) || y.Matricula == filter.Matricula) &&
-                (filter.SituacaoId == null || !filter.SituacaoId.Any() || filter.SituacaoId.Contains(y.TipoStatusAlunoId))
-            ));
+            if (!string.IsNullOrEmpty(filter.CodigoTurma) || filter.CursoId.HasValue || !string.IsNullOrEmpty(filter.Matricula) || (filter.SituacaoId != null && filter.SituacaoId.Any()))
+            {
+                query = query.Where(x => x.TurmasAluno.Any(y =>
+                    (string.IsNullOrEmpty(filter.CodigoTurma) || y.Turma.Codigo.Contains(filter.CodigoTurma)) &&
+                    (!filter.CursoId.HasValue || y.Turma.CursoId == filter.CursoId) &&
+                    (string.IsNullOrEmpty(filter.Matricula) || y.Matricula == filter.Matricula) &&
+                    (filter.SituacaoId == null || !filter.SituacaoId.Any() || filter.SituacaoId.Contains(y.TipoStatusAlunoId))
+                ));
+            }
 
             if (paginar)
             {
